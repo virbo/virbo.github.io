bbd80b4b26226bbc56511687c06f9da0222f7f14
Yusuf Ayuba <yusufayuba@live.com>
2018-12-26 21:56:48 +0800

    init

---
+++ precache-manifest.3987135e5ebf47a66ee37063c5dfbe70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "103c3834390702eab891",
+    "url": "/static/js/main.103c3834.chunk.js"
+  },
+  {
+    "revision": "9ce6370c3abb8e1b8c58",
+    "url": "/static/js/1.9ce6370c.chunk.js"
+  },
+  {
+    "revision": "103c3834390702eab891",
+    "url": "/static/css/main.c1e3ab3b.chunk.css"
+  },
+  {
+    "revision": "9ce6370c3abb8e1b8c58",
+    "url": "/static/css/1.4f1b1d94.chunk.css"
+  },
+  {
+    "revision": "23a9882fbfabdbe17d518628a23fe08f",
+    "url": "/index.html"
+  }
+];
