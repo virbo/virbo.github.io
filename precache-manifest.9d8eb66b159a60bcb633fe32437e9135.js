40fba85433bf9fbf8f1ad21d89228f7d2e7910bd
Yusuf Ayuba <yusufayuba@live.com>
2019-05-17 21:04:08 +0800

    #9, add new asset

---
+++ precache-manifest.9d8eb66b159a60bcb633fe32437e9135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7a392a595e2fd715cdb",
+    "url": "/static/css/main.f3811ffb.chunk.css"
+  },
+  {
+    "revision": "d7a392a595e2fd715cdb",
+    "url": "/static/js/main.38deb301.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3370c643dc10e97b5fe0",
+    "url": "/static/css/2.2a899dcc.chunk.css"
+  },
+  {
+    "revision": "3370c643dc10e97b5fe0",
+    "url": "/static/js/2.f73a77cf.chunk.js"
+  },
+  {
+    "revision": "e595794c507f1daf678987aff402eccb",
+    "url": "/static/media/playstore.e595794c.png"
+  },
+  {
+    "revision": "03a5e5aa969ef0c482a8bb03d8773802",
+    "url": "/index.html"
+  }
+];
